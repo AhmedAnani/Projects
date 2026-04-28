333ab2f0bfb637013bed0fd1efb5c36ae1a35879
Ahmed Anani <111708885+AhmedAnani@users.noreply.github.com>
2026-04-28 17:13:38 +0300

    Update Book.cs

--- Project/src/Models/Book.cs
+++ Project/src/Models/Book.cs
@@ -10,58 +10,62 @@ using System.Threading.Tasks;
 public class Book : BookItem, IBorrowable, IBuyable
 {
     private DateTime _dueDate;
-    public DateTime DueDate
+public DateTime DueDate
+{
+    get => _dueDate;
+    set
     {
-        get => _dueDate;
-        set
-        {
-            if (value < DateTime.Now) throw new Exception("Due date cannot be in the past");
-            _dueDate = value;
-        }
+        if (value < DateTime.Now) throw new Exception("Due date cannot be in the past");
+        _dueDate = value;
     }
-    public Book(int id, string title, bool isAvailable, string author, string description, BookCategory category) : base(id, title, isAvailable, author, description, category)
+}
+
+public Book(int id, string title, bool isAvailable, string author, string description, BookCategory category) : base(id, title, isAvailable, author, description, category)
+{
+}
+public override void displayInfo()
+{
+    base.displayInfo();// Call the base method to display common information
+    
+}
+
+public void BorrowItem()
+{
+    if (!IsAvailable)
     {
+        Console.WriteLine("Sorry, this book is currently not available for borrowing.");
+        return;
     }
-
-    public void BorrowItem()
+    else
     {
-        if (!IsAvailable)
-        {
-            Console.WriteLine("Sorry, this book is currently not available for borrowing.");
-            
-        }
-        else
-        {
-            IsAvailable = false;
-            Console.WriteLine($"You have borrowed the book: {Title}");
-        }
+        IsAvailable = false;
+        Console.WriteLine($"You have borrowed the book: {Title}");
     }
+}
 
-    public void ReturnItem()
+public void ReturnItem()
+{
+    if (IsAvailable)
     {
-        if (IsAvailable)
-        {
-            throw new Exception("You cannot return an item that you haven't borrowed.");
-
-        }
-        else
-        {
-            IsAvailable = true;
-            Console.WriteLine($"You have returned the book: {Title}");
-        }
+        Console.WriteLine("This book is already available in the library.");
+        return;
     }
+    IsAvailable = true;
+    Console.WriteLine($"You have returned the book: {Title}");
+}
 
-    public void BuyItem()
+public void BuyItem()
+{
+    if (IsAvailable)
     {
-        if (IsAvailable)
-        {
-            IsAvailable = false;
-            Console.WriteLine($"You have bought the book: {Title}");
-        }
-        else
-        {
-            throw new Exception("Sorry, this book is currently not available for buying.");
-        }
+        IsAvailable = false;
+        Console.WriteLine($"You have bought the book: {Title}");
+    }
+    else
+    {
+        Console.WriteLine("Sorry, this book is currently not available for buying.");
+    }
+}
     }
 }
 
