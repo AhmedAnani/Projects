b93b46030f9369bbe844d8e4d6cb7743270287d6
Ahmed Anani <111708885+AhmedAnani@users.noreply.github.com>
2026-04-28 17:19:54 +0300

    Refactor BookItem class for improved structure

--- Project/src/Models/BookItem.cs
+++ Project/src/Models/BookItem.cs
@@ -1,67 +1,72 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
-public abstract class BookItem : LibraryItem
+namespace Project.src.Models
 {
-
-    private string _author;
-
-    private string _description;
-
-    private BookCategory _category;
-
-
-    public string Author
+    public abstract class BookItem : LibraryItem
     {
-        get => _author;
-        set
+
+        private string _author;
+
+        private string _description;
+
+        private BookCategory _category;
+
+
+        public string Author
         {
-            if (string.IsNullOrEmpty(value)) throw new Exception("Author cannot be null or empty");
-
-            _author = value;
-        }
-    }
-
-    public string Description
-    {
-        get => _description;
-        set
-        {
-            _description = value;
-        }
-    }
-
-    public BookCategory Category
-    {
-        get => _category;
-
-        set
-        {
-            if (!Enum.IsDefined(typeof(BookCategory), value)) //Check if the value is a valid 
+            get => _author;
+            set
             {
-                string Categoryes = string.Join(", ", Enum.GetNames<BookCategory>()); //Get the allowed Categoryes from the enum and add it  them into a string
-                throw new IndexOutOfRangeException($"Invalid category: {value}. Allowed values are: {Categoryes}");
+                if (string.IsNullOrEmpty(value)) throw new Exception("Author cannot be null or empty");
+
+                _author = value;
             }
-
-            _category = value;
         }
-    }
 
-    public BookItem(int id, string title, bool isAvailable, string author, string description, BookCategory category) : base(id, title, isAvailable)
-    {
-        Author = author;
-        Description = description;
-        Category = category;
-    }
+        public string Description
+        {
+            get => _description;
+            set
+            {
+                _description = value;
+            }
+        }
 
-    public override void displayInfo()
-    {
-        Console.WriteLine($"Id: {Id}");
-        Console.WriteLine($"Title: {Title}");
-        Console.WriteLine($"Author: {Author}");
-        Console.WriteLine($"Description: {Description}");
-        Console.WriteLine($"Category: {Category}");
-        Console.WriteLine($"Is Available: {(IsAvailable ? "Yes" : "No")}");
+        public BookCategory Category
+        {
+            get => _category;
+
+            set
+            {
+                if (!Enum.IsDefined(typeof(BookCategory), value)) //Check if the value is a valid 
+                {
+                    string Categoryes = string.Join(", ", Enum.GetNames<BookCategory>()); //Get the allowed Categoryes from the enum and add it  them into a string
+                    throw new IndexOutOfRangeException($"Invalid category: {value}. Allowed values are: {Categoryes}");
+                }
+
+                _category = value;
+            }
+        }
+
+        public BookItem(int id, string title, bool isAvailable, string author, string description, BookCategory category) : base(id, title, isAvailable)
+        {
+            Author = author;
+            Description = description;
+            Category = category;
+        }
+
+        public override void displayInfo()
+        {
+            Console.WriteLine($"Id: {Id}");
+            Console.WriteLine($"Title: {Title}");
+            Console.WriteLine($"Author: {Author}");
+            Console.WriteLine($"Description: {Description}");
+            Console.WriteLine($"Category: {Category}");
+            Console.WriteLine($"Is Available: {(IsAvailable ? "Yes" : "No")}");
+
+
+        }
     }
 }
