f7b993fc006251f9ecb03e3a50e2aa1179817b2b
Ahmed Anani <111708885+AhmedAnani@users.noreply.github.com>
2026-04-28 17:16:26 +0300

    Update User.cs

--- Project/src/Models/User.cs
+++ Project/src/Models/User.cs
@@ -1,28 +1,74 @@
-﻿using System;
+using Project.src.Enums;
+using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
-
-public class User
+namespace Project.src.Models
 {
-    private string _name;
-    public string Name
+    internal class User
     {
-        get => _name;
-        set
+        private int _id;
+
+        private string _name;
+
+        private string _email;
+
+        private UserRole _role;
+
+        public int Id { get => _id;
+            set{ 
+                if(value <= 0) throw new Exception($"ID must be a positive integer not{value}");
+
+                _id = value;
+            } }
+
+        public string Name { get => _name;  
+            set {
+                if(string.IsNullOrEmpty(value)) throw new Exception("Name cannot be null or empty");
+
+                _name = value; 
+            } }
+
+        public string Email
         {
-            if (string.IsNullOrEmpty(value)) throw new Exception("Name cannot be null or empty");
-            _name = value;
+            get=> _email;
+            set
+            {
+                if (string.IsNullOrEmpty(value) || !value.Contains("@"))
+                {
+                    throw new Exception("Invalid email format");
+                }
+                _email = value;
+            }
         }
-    }
-    public User(string name)
-    {
-        Name = name;
-    }
-    public List<LibraryItem> BorrowedItems { get; set; } = new List<LibraryItem>();
-    public List<LibraryItem> BoughtItems { get; set; } = new List<LibraryItem>();
+        
+         
 
+        
+
+        public UserRole Role
+        {
+            get => _role;
+            set
+            {
+                if (!Enum.IsDefined(typeof(UserRole), value))
+                {
+                    string roles = string.Join(", ", Enum.GetNames<UserRole>());
+                    throw new Exception($"Invalid role: {value}. Allowed values are: {roles}");
+                }
+                _role = value;
+            }
+        }
+
+        public User(int id, string name, string email,UserRole role)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            Role = role;
+        }
+        public List<LibraryItem> BorrowedItems { get; set; } = new List<LibraryItem>();
+        public List<LibraryItem> BoughtItems { get; set; } = new List<LibraryItem>();
+
+    }
 }
-
