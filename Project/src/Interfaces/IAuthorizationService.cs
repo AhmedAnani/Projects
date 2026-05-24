e9c43eef6a67581cf73e15aef82011353967da17
MohamedAbdAlrhmn <104036993+MohamedAbdAlrhmn@users.noreply.github.com>
2026-05-24 17:41:46 +0300

    AuthorizationService done

---
+++ Project/src/Interfaces/IAuthorizationService.cs
@@ -0,0 +1,21 @@
+﻿using Project.src.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.src.Interfaces
+{
+    public interface IAuthorizationService
+    {
+        bool CanManageUsers(User? user);
+        bool CanViewReports(User? user);
+        bool CanAddItems(User? user);
+        bool CanUpdateItems(User? user);
+        bool CanDeleteItems(User? user);
+        bool CanViewItems(User? user);
+        bool CanBorrow(User? user);
+        bool CanBuy(User? user);
+    }
+}
---
+++ Project/src/Services/AuthorizationService.cs
@@ -0,0 +1,50 @@
+﻿using Project.src.Enums;
+using Project.src.Interfaces;
+using Project.src.Models;
+using System.Linq;
+
+
+namespace Project.src.Services
+{
+    public class AuthorizationService : IAuthorizationService
+    {
+
+        //This method used to check if the user has the required role to perform a specific action
+        private static bool HasRole(User? user, params UserRole[] allowedRoles)
+        {
+            return user != null && allowedRoles.Contains(user.Role);
+        }
+
+        //This method used to manage users (Admin only can manage users)
+        public bool CanManageUsers(User? user) => HasRole(user, UserRole.Admin);
+
+
+        //This method used to give permission to view reports in the system (Admin only can view reports)
+        public bool CanViewReports(User? user) => HasRole(user, UserRole.Admin);
+     
+
+        //This method used to give permission to Add Items in the system (Employee and Admin can add items)
+        public bool CanAddItems(User? user) => HasRole(user,UserRole.Admin, UserRole.Employee);
+       
+
+        //This method used to give permission to Update Items in the system (Employee and Admin can update items)
+        public bool CanUpdateItems(User? user) => HasRole(user, UserRole.Admin, UserRole.Employee);
+
+
+        //This method used to give permission to delete Items in the system (Employee and Admin can delete items)
+        public bool CanDeleteItems(User? user) => HasRole(user, UserRole.Admin, UserRole.Employee);
+
+
+        //This method used to give permission to view Items in the system (Employee, Admin, and User can view items)
+        public bool CanViewItems(User? user) => HasRole(user, UserRole.Admin, UserRole.Employee, UserRole.User);
+
+
+        //This method used to give permission to borrow Items in the system (Employee and User can borrow items)
+        public bool CanBorrow(User? user) => HasRole(user, UserRole.Employee, UserRole.User);
+
+
+        //This method used to give permission to purchase Items in the system (Employee and User can purchase items)
+        public bool CanBuy(User? user) => HasRole(user, UserRole.Employee, UserRole.User);
+
+    }
+}
